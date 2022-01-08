dff22a6426acdf202aeea02291d4d70366b6b572
Michael <m.muenz@gmail.com>
2022-01-08 19:35:12 +0100

    Update setup.sh

--- net/wireguard/src/opnsense/scripts/OPNsense/Wireguard/setup.sh
+++ net/wireguard/src/opnsense/scripts/OPNsense/Wireguard/setup.sh
@@ -2,3 +2,7 @@
 
 mkdir -p /var/run/wireguard
 chmod 755 /var/run/wireguard
+
+# Create folder for client export and wipe it with every restart to track changes
+mkdir -p /usr/local/etc/wireguard/clientexport
+rm -f /usr/local/etc/wireguard/clientexport/*
