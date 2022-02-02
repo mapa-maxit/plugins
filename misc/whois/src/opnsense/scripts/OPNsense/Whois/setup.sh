e00d35e8dcce7383d4edfa1493082818b845b7bb
Michael <m.muenz@gmail.com>
2022-02-02 17:06:43 +0100

    Delete setup.sh

--- misc/whois/src/opnsense/scripts/OPNsense/Whois/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p /var/run/Whois
-chown -R Whois:Whois /var/run/Whois
-chmod 755 /var/run/Whois
-
-mkdir -p /var/lib/Whois
-chown -R Whois:Whois /var/lib/Whois
-chmod 755 /var/lib/Whois /var/lib
