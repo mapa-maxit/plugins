8bd78365e222df083352d8c53c77d2b58f82c804
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-02-04 13:04:37 +0100

    Update General.php

--- misc/whois/src/opnsense/mvc/app/models/OPNsense/Whois/General.php
+++ misc/whois/src/opnsense/mvc/app/models/OPNsense/Whois/General.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Copyright (C) 2022 Manuel Pasti <mapa@maxit.de>
+    Copyright (C) 2022 Manuel Pasti <mapa@max-it.de>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
