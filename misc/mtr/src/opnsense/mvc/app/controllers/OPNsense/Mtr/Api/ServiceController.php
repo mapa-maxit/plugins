da6d44d47e1bbf205e202629b8abe2bf7e1dcdbb
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-02-25 15:45:17 +0100

    wef
    fe

---
+++ misc/mtr/src/opnsense/mvc/app/controllers/OPNsense/Mtr/Api/ServiceController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Copyright (C) 2022 Manuel Pasti <mapa@max-it.de>
+ * All rights reserved.
+ */
+namespace OPNsense\Mtr\Api;
+
+use OPNsense\Base\ApiMutableServiceControllerBase;
+use OPNsense\Core\Backend;
+use OPNsense\Mtr\General;
+
+/**
+ * Class ServiceController
+ * @package OPNsense\Mtr
+ */
+class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutableServiceControllerBase*/
+{
+    protected static $internalServiceClass = '\OPNsense\Mtr\General';
+    protected static $internalServiceTemplate = 'OPNsense/Mtr';
+    protected static $internalServiceEnabled = 'enabled';
+    protected static $internalServiceName = 'mtr';
+
+    public function ipAction()
+    {
+      if ($this->request->isPost()) {
+        $backend = new Backend();
+        $mdlGeneral = new General();
+        $ipaddress = $mdlGeneral->ip;
+        
+        $ipadi = escapeshellarg($this->request->getPost("ipadd"));
+        
+        $response = $backend->configdRun("mtr ip $ipadi");
+        return array("response" => $response);
+      }
+    }
+}
---
+++ misc/mtr/src/opnsense/mvc/app/models/OPNsense/Mtr/General.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Copyright (C) 2022 Manuel Pasti <mapa@max-it.de>
+ * All rights reserved.
+ */
+
+namespace OPNsense\Mtr;
+
+use OPNsense\Base\BaseModel;
+
+class General extends BaseModel
+{
+}
