5784da079a80dca4c124d0f48f47370bc4546f47
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-01 10:08:02 +0100

    Update ServiceController.php

--- misc/mtr/src/opnsense/mvc/app/controllers/OPNsense/Mtr/Api/ServiceController.php
+++ misc/mtr/src/opnsense/mvc/app/controllers/OPNsense/Mtr/Api/ServiceController.php
@@ -29,6 +29,7 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutab
         $ipaddress = $mdlGeneral->ip;
         
         $ipadi = escapeshellarg($this->request->getPost("ipadd"));
+        $ipturns = escapeshellarg($this->request->getPost("ipcount"));
         
         $response = $backend->configdRun("mtr ip $ipadi");
         return array("response" => $response);
