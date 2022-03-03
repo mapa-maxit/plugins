9663f307a7a5663b533c0587f6429c1c56c2a222
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-03 15:08:06 +0100

    Update ServiceController.php

--- misc/mtr/src/opnsense/mvc/app/controllers/OPNsense/Mtr/Api/ServiceController.php
+++ misc/mtr/src/opnsense/mvc/app/controllers/OPNsense/Mtr/Api/ServiceController.php
@@ -36,8 +36,7 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutab
 
 
         if($ipturns == "1"){
-            if($ipj = 'on'){
-                echo "test";
+            if($ipj == 'on'){
                 $response = $backend->configdRun("mtr ip1j $ipadi");
                 return array("response" => $response);
             }elseif($ipreverse == "y"){
