ec9cb19eab1ec6c0307ecabe5204f8d2094c198a
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-08 10:38:53 +0100

    Update ServiceController.php

--- misc/whois/src/opnsense/mvc/app/controllers/OPNsense/Whois/Api/ServiceController.php
+++ misc/whois/src/opnsense/mvc/app/controllers/OPNsense/Whois/Api/ServiceController.php
@@ -14,7 +14,7 @@ use OPNsense\Whois\General;
  * Class ServiceController
  * @package OPNsense\Whois
  */
-class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutableServiceControllerBase*/
+class ServiceController extends ApiMutableServiceControllerBase
 {
     protected static $internalServiceClass = '\OPNsense\Whois\General';
     protected static $internalServiceTemplate = 'OPNsense/Whois';
