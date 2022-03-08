0d0b9f93ff69fd0ee4aeeea7b5e45c2d6ce4a87d
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-08 13:59:47 +0100

    Update ServiceController.php

--- misc/whois/src/opnsense/mvc/app/controllers/OPNsense/Whois/Api/ServiceController.php
+++ misc/whois/src/opnsense/mvc/app/controllers/OPNsense/Whois/Api/ServiceController.php
@@ -29,7 +29,7 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutab
         $ipenable = $mdlGeneral->enabled;
 
         if($ipenable == "1"){
-            $response = $backend->configdRun("whois general.ip $ipaddress");
+            $response = $backend->configdRun("whois ip $ipaddress");
             return array("response" => $response);
         }else{
             $response = $backend->configdRun("whois $ipaddress");
