9254bbeca32be9b10967c44f82bacc13ff94f494
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-11 10:02:23 +0100

    Update ServiceController.php

--- misc/nmap/src/opnsense/mvc/app/controllers/OPNsense/Nmap/Api/ServiceController.php
+++ misc/nmap/src/opnsense/mvc/app/controllers/OPNsense/Nmap/Api/ServiceController.php
@@ -4,7 +4,7 @@
  * Copyright (C) 2022 Manuel Pasti <mapa@max-it.de>
  * All rights reserved.
  */
-namespace OPNsense\Mtr\Api;
+namespace OPNsense\Nmap\Api;
 
 use OPNsense\Base\ApiMutableServiceControllerBase;
 use OPNsense\Core\Backend;
@@ -12,14 +12,14 @@ use OPNsense\Mtr\General;
 
 /**
  * Class ServiceController
- * @package OPNsense\Mtr
+ * @package OPNsense\Nmap
  */
 class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutableServiceControllerBase*/
 {
-    protected static $internalServiceClass = '\OPNsense\Mtr\General';
-    protected static $internalServiceTemplate = 'OPNsense/Mtr';
+    protected static $internalServiceClass = '\OPNsense\Nmap\General';
+    protected static $internalServiceTemplate = 'OPNsense/Nmap';
     protected static $internalServiceEnabled = 'enabled';
-    protected static $internalServiceName = 'mtr';
+    protected static $internalServiceName = 'nmap';
 
     public function ipAction()
     {
@@ -36,55 +36,55 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutab
 
         if($ipturns == "1"){
             if($ipreverse == "1"){
-                $response = $backend->configdRun("mtr ip1n $ipadi");
+                $response = $backend->configdRun("nmap ip1n $ipadi");
                 return array("response" => $response);
             }else{
-                $response = $backend->configdRun("mtr ip1 $ipadi");
+                $response = $backend->configdRun("nmap ip1 $ipadi");
                 return array("response" => $response);
             }
             
         }elseif($ipturns == "2"){
             if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("mtr ip1n $ipadi");
+                $response = $backend->configdRun("nmap ip1n $ipadi");
                 return array("response" => $response);
             }else{
-                $response = $backend->configdRun("mtr ip2 $ipadi");
+                $response = $backend->configdRun("nmap ip2 $ipadi");
                 return array("response" => $response);
             }
             
         }elseif($ipturns == "3"){
             if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("mtr ip1n $ipadi");
+                $response = $backend->configdRun("nmap ip1n $ipadi");
                 return array("response" => $response);
             }else{
-                $response = $backend->configdRun("mtr ip3 $ipadi");
+                $response = $backend->configdRun("nmap ip3 $ipadi");
                 return array("response" => $response);
             }
             
         }elseif($ipturns == "4"){
             if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("mtr ip1n $ipadi");
+                $response = $backend->configdRun("nmap ip1n $ipadi");
                 return array("response" => $response);
             }else{
-                $response = $backend->configdRun("mtr ip4 $ipadi");
+                $response = $backend->configdRun("nmap ip4 $ipadi");
                 return array("response" => $response);
             }
             
         }elseif($ipturns == "5"){
             if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("mtr ip1n $ipadi");
+                $response = $backend->configdRun("nmap ip1n $ipadi");
                 return array("response" => $response);
             }else{
-                $response = $backend->configdRun("mtr ip5 $ipadi");
+                $response = $backend->configdRun("nmap ip5 $ipadi");
                 return array("response" => $response);
             }
             
         }else{
             if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("mtr ip1n $ipadi");
+                $response = $backend->configdRun("nmap ip1n $ipadi");
                 return array("response" => $response);
             }else{
-                $response = $backend->configdRun("mtr ip1 $ipadi");
+                $response = $backend->configdRun("nmap ip1 $ipadi");
                 return array("response" => $response);
             }
         }
