47218ea35a7c8552004524f86c19efa0b91d8963
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-11 13:52:52 +0100

    Update ServiceController.php

--- misc/nmap/src/opnsense/mvc/app/controllers/OPNsense/Nmap/Api/ServiceController.php
+++ misc/nmap/src/opnsense/mvc/app/controllers/OPNsense/Nmap/Api/ServiceController.php
@@ -31,24 +31,57 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController
         $ipadi = escapeshellarg($this->request->getPost("ipadd")); 
         $ipturns = $mdlGeneral->turn;
         $ipreverse = $mdlGeneral->drl;
+        $ipver = $mdlGeneral->ver;
           
-          
-        if($ipturns == "Pingscan" && $ipreverse == "1"){
-        $response = $backend->configdRun("nmap ip1 $ipadi");
-        return array("response" => $response);
+        if($ipturns == "Pingscan" && $ipreverse == "1" && $ipreverse == "1"){
+            $response = $backend->configdRun("nmap ip11 $ipadi");
+            return array("response" => $response);
+            
+        }elseif($ipturns == "SYNScan" && $ipreverse == "1" && $ipreverse == "1"){
+            $response = $backend->configdRun("nmap ip10 $ipadi");
+            return array("response" => $response);
+            
+        }elseif($ipturns == "UDPscan" && $ipreverse == "1" && $ipreverse == "1"){
+            $response = $backend->configdRun("nmap ip12 $ipadi");
+            return array("response" => $response);
+            
+        }elseif($ipturns == "Pingscan" && $ipreverse == "1"){
+            $response = $backend->configdRun("nmap ip1 $ipadi");
+            return array("response" => $response);
         
         }elseif($ipturns == "SYNScan" && $ipreverse == "1"){
-        $response = $backend->configdRun("nmap ip2 $ipadi");
-        return array("response" => $response);
+            $response = $backend->configdRun("nmap ip2 $ipadi");
+            return array("response" => $response);
+            
+        }elseif($ipturns == "UDPscan" && $ipreverse == "1"){
+            $response = $backend->configdRun("nmap ip6 $ipadi");
+            return array("response" => $response);
+            
+        }elseif($ipturns == "Pingscan" && $ipver == "1"){
+            $response = $backend->configdRun("nmap ip8 $ipadi");
+            return array("response" => $response);
+        
+        }elseif($ipturns == "SYNScan" && $ipver == "1"){
+            $response = $backend->configdRun("nmap ip7 $ipadi");
+            return array("response" => $response);
+         
+        }elseif($ipturns == "UDPscan" && $ipver == "1"){
+            $response = $backend->configdRun("nmap ip9 $ipadi");
+            return array("response" => $response);
+            
+        }elseif($ipturns == "Pingscan"){
+            $response = $backend->configdRun("nmap ip4 $ipadi");
+            return array("response" => $response);
+            	}        
 
         }elseif($ipturns == "SYNScan"){
-        $response = $backend->configdRun("nmap ip3 $ipadi");
-        return array("response" => $response);
+            $response = $backend->configdRun("nmap ip3 $ipadi");
+            return array("response" => $response);
 
-        }elseif($ipturns == "Pingscan"){
-        $response = $backend->configdRun("nmap ip4 $ipadi");
-        return array("response" => $response);
-            	}
+        }elseif($ipturns == "UDPscan"){
+            $response = $backend->configdRun("nmap ip5 $ipadi");
+            return array("response" => $response);
+            
         }
     }
 }
