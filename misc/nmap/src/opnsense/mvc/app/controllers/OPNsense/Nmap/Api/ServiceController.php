232cc3284fc991f921a1cac33f399470468f5e30
mapa-maxit <98880202+mapa-maxit@users.noreply.github.com>
2022-03-11 10:55:11 +0100

    Update ServiceController.php

--- misc/nmap/src/opnsense/mvc/app/controllers/OPNsense/Nmap/Api/ServiceController.php
+++ misc/nmap/src/opnsense/mvc/app/controllers/OPNsense/Nmap/Api/ServiceController.php
@@ -31,70 +31,10 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController /*ApiMutab
         $ipadi = escapeshellarg($this->request->getPost("ipadd")); 
         $ipturns = ($this->request->getPost("ipcount"));
         $ipreverse = $mdlGeneral->drl;
-<<<<<<< HEAD
-  
 
-
-        if($ipturns == "1"){
-            if($ipreverse == "1"){
-                $response = $backend->configdRun("nmap ip1n $ipadi");
-                return array("response" => $response);
-            }else{
-                $response = $backend->configdRun("nmap ip1 $ipadi");
-                return array("response" => $response);
-            }
-            
-        }elseif($ipturns == "2"){
-            if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("nmap ip1n $ipadi");
-                return array("response" => $response);
-            }else{
-                $response = $backend->configdRun("nmap ip2 $ipadi");
-                return array("response" => $response);
-            }
-            
-        }elseif($ipturns == "3"){
-            if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("nmap ip1n $ipadi");
-                return array("response" => $response);
-            }else{
-                $response = $backend->configdRun("nmap ip3 $ipadi");
-                return array("response" => $response);
-            }
-            
-        }elseif($ipturns == "4"){
-            if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("nmap ip1n $ipadi");
-                return array("response" => $response);
-            }else{
-                $response = $backend->configdRun("nmap ip4 $ipadi");
-                return array("response" => $response);
-            }
-            
-        }elseif($ipturns == "5"){
-            if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("nmap ip1n $ipadi");
-                return array("response" => $response);
-            }else{
-                $response = $backend->configdRun("nmap ip5 $ipadi");
-                return array("response" => $response);
-            }
-            
-        }else{
-            if($ipreverse == "1" && $ipreverse == "1"){
-                $response = $backend->configdRun("nmap ip1n $ipadi");
-                return array("response" => $response);
-            }else{
-                $response = $backend->configdRun("nmap ip1 $ipadi");
-                return array("response" => $response);
-            }
-        }
-=======
-          
         $response = $backend->configdRun("nmap ip $ipadi");
         return array("response" => $response);
->>>>>>> 7946c9663e14e026d7e28301e2b16325a726f005
-      }
+        }
     }
 }
 ?>
